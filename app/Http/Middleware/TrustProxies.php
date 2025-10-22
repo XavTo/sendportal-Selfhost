47ed30bb8d6eccca9083188f1884ee8a92504fb5
Xavier To <xaviertonneller6@outlook.fr>
2025-10-22 21:38:09 +0200

    [FIX] fix proxy for Railway

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -8,14 +8,15 @@ use Illuminate\Http\Request;
 class TrustProxies extends Middleware
 {
     /**
-     * The trusted proxies for this application.
+     * Faire confiance aux proxys en amont (Railway).
+     * Vous pouvez restreindre à une liste d’IP si besoin.
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
-     * The headers that should be used to detect proxies.
+     * En-têtes à honorer pour détecter schéma/hôte/port/IP réels.
      *
      * @var int
      */
